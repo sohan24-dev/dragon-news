df9ab4908555211221bae80213619fdb3e1c0fa0
sohan24-dev <sohan.explorer@gmail.com>
2026-04-26 19:52:10 +0600

    solve

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: BETTER_AUTH_URL
+    baseURL: BETTER_AUTH_URL,
     
 })
 
